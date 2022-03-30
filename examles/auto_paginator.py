7e4a219d5c867df50ffa978a7fded6a986db2804
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2022-03-30 09:58:06 +0200

    Create auto_paginator.py

---
+++ examles/auto_paginator.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import DiscordUtils
+
+bot = commands.Bot(command_prefix=">")
+
+@bot.command()
+async def paginate(ctx):
+    embed1 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 1")
+    embed2 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 2")
+    embed3 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 3")
+    paginator = DiscordUtils.Pagination.AutoEmbedPaginator(ctx)
+    embeds = [embed1, embed2, embed3]
+    await paginator.run(embeds)
+
+bot.run("token")
