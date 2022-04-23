76f0f11f3e6e1e9d3acfdd197d055107f9edd151
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2022-04-23 17:44:54 +0200

    Update __init__.py

--- disutils/__init__.py
+++ disutils/__init__.py
@@ -4,6 +4,6 @@ from .InviteTracker import InviteTracker
 from .Music import Music, EmptyQueue, NotConnectedToVoice, NotPlaying
 
 __title__ = "disutils"
-__version__ = "1.3.4"
+__version__ = "1.4"
 __author__ = "toxicrecker"
 __license__ = "MIT"
