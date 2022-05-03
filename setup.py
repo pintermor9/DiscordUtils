7d12676c21f6e8470e1d7e7442dce8b07c093af1
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2022-05-03 09:33:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import re
 
-dependencies = ["discord.py", "aiohttp"]
+dependencies = ["aiohttp"]
 
 with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
     long_description = f.read()
