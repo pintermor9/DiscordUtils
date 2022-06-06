0c2d5dff39b17d8d9b04ba459b5d8b315fcbc4c8
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2022-06-06 14:11:20 +0200

    typo

--- disutils/__init__.py
+++ disutils/__init__.py
@@ -6,7 +6,7 @@ from .Managers.MusicManager import MusicManager, MusicPlayer, get_video_data, So
 from .Managers.LevellingManager import LevellingManager
 
 __title__ = "disutils"
-__version__ = "1.4.32.post2a"
+__version__ = "1.4.32.post2"
 __author__ = "pintermor9"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2018-2022 toxicrecker\nCopyright (c) 2022-present pintermor9"
